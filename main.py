b241492608459d4f5b8dcbf9e920645ab2528937
Saankhya Mondal <saankhya1997@gmail.com>
2023-09-14 01:55:16 +0530

    Model Evaluation Pipeline Created

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTraini
 from text_summarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from text_summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from text_summarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from text_summarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 from text_summarizer.logging import logger
 
@@ -44,6 +45,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_trainer = ModelTrainerTrainingPipeline()
    model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
